221a9301b324ee3d85fdf95bd31a80142bb58b9a
potistudio2022 <potistudio2022@gmail.com>
2022-10-10 12:30:31 +0900

    Loading Card

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,5 +1,17 @@
 // Import ipcRenderer
 const { ipcRenderer } = require ("electron");
 
+/* Import External Modules */
+const anime = require ("animejs");
+
 document.getElementById ("windowMinimizeButton").addEventListener ("click", () => ipcRenderer.send("minimize-window"));
 document.getElementById ("windowCloseButton").addEventListener ("click", () => ipcRenderer.send("close-window"));
+
+ipcRenderer.on ("music-all-loaded", () => {
+	anime ({
+		targets: ".Loading-Music",
+		translateY: -20,
+		opacity: 0,
+		duration: 1000
+	});
+});
